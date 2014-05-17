6cb1d7993a0bd9ccd2fc2bd35a188b9e55c09124
Peter <peterhsaxton@gmail.com>
2014-05-17 17:46:18 +0100

    freeze tests

--- spec/compositions_spec.js
+++ spec/compositions_spec.js
@@ -6,7 +6,7 @@ describe('compositions', function(){
       }).toThrow("Can't find variable: limit");
     });
     it('should make functions available on top namespace', function(){
-      _.expose('weed limit '
+      _.expose('weed limit all '
               );
       expect(function(){
         limit();
@@ -14,6 +14,7 @@ describe('compositions', function(){
       }).not.toThrow();
     });
   });
+
   describe('limit by keys', function(){
     var obj;
     beforeEach(function(){
@@ -21,15 +22,15 @@ describe('compositions', function(){
     });
     it('should restrict on keys', function(){
       var limited = limit('a')(obj);
-      expect(limited).toEqual({a: 1});
+      expect(limited).toEqual(Object.freeze({a: 1}));
     });
     it('should restrict on multiple keys', function(){
       var limited = limit('a', 'b')(obj);
-      expect(limited).toEqual({a: 1, b: 2});
+      expect(limited).toEqual(Object.freeze({a: 1, b: 2}));
     });
     it('should not add values not on original', function(){
       var limited = limit('a', 'd')(obj);
-      expect(limited).toEqual({a: 1});
+      expect(limited).toEqual(Object.freeze({a: 1}));
     });
   });
 
@@ -40,15 +41,27 @@ describe('compositions', function(){
     });
     it('should restrict on keys', function(){
       var weeded = weed('a')(obj);
-      expect(weeded).toEqual({b: 2, c: 3});
+      expect(weeded).toEqual(Object.freeze({b: 2, c: 3}));
     });
     it('should restrict on multiple keys', function(){
       var weeded = weed('a', 'b')(obj);
-      expect(weeded).toEqual({c: 3});
+      expect(weeded).toEqual(Object.freeze({c: 3}));
     });
     it('should not add values not on original', function(){
       var weeded = weed('a', 'd')(obj);
-      expect(weeded).toEqual({b: 2, c: 3});
+      expect(weeded).toEqual(Object.freeze({b: 2, c: 3}));
+    });
+  });
+
+  describe('all', function(){
+    it('should check if all satisfy condition', function(){
+      function modulo(denominator){
+        return function(number){
+          return number % denominator;
+        };
+      }
+      var divisiblyBy2 = modulo(2);
+      expect(all(divisiblyBy2)([2,4,6])).toEqual(true);
     });
   });
 });

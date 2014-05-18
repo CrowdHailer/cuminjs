d742892cf28463150fd8e5f69e0fd388b1aedbb1
Peter <peterhsaxton@gmail.com>
2014-05-18 10:46:37 +0100

    grunt tasks added

---
+++ dist/cuminjs.js
@@ -0,0 +1,214 @@
+argsToList = function(x){
+  return Array.prototype.slice.call(x);
+}
+
+isArray = function(obj) {
+  return (obj && obj.constructor == Array);
+}
+
+isObj = function(obj) {
+  return (typeof obj == "object" && !isArray(obj));
+}
+// Definitions
+// Arrays have index, element
+// Objects have key, value
+// Colletions have location, item
+
+// uses underscore namespace,
+// provides duplicate functionality for underscore.
+
+// functions return frozen objects
+var _ = (function(){
+
+// basic iterators
+
+  function eachArr(operation){
+  // iterates array left to right
+  // assumes array type
+    return function(arr){
+      var context = {empty: []};
+      for (var i = 0; i < arr.length; i++) {
+        item = arr[i];
+        operation.call(context, item, i);
+      }
+    };
+  }
+
+  function eachArrRight(operation){
+  // iterates array right to left
+  // assumes array type
+    return function(arr){
+      var context = {empty: []};
+      for (var index = arr.length - 1; index > -1; index--) {
+        item = arr[index];
+        operation.call(context, item, index);
+      }
+    };
+  }
+
+  function eachObj(operation){
+  // iterates through object key/value pairs
+  // no order assumed
+    return function(obj){
+      var keys = Object.keys(obj);
+      var context = {empty: {}};
+      eachArr(function(key){
+        var value = obj[key];
+        operation.call(context, value, key);
+      })(keys);
+    };
+  }
+
+  function each(operation){
+  // iterates through object/array/arguments
+  // iterates left to right when given array/arguments
+    return function(collection){
+      if (arguments.length > 1) {
+        collection = argsToList(arguments);
+      }
+      if (isArray(collection)) {
+        eachArr(operation)(collection);
+      } else {
+        eachObj(operation)(collection);
+      }
+    };
+  }
+
+// Basic collection operations
+
+  function map(operation){
+    return function(){
+      var results;
+      each(function(item, location){
+        results = this.empty;
+        results[location] = operation.call({}, item, location);
+      }).apply({}, arguments);
+      return Object.freeze(results);
+    };
+  }
+
+  function filter(operation){
+    return function(){
+      var results;
+      each(function(item, location){
+        results = this.empty;
+        if (operation.call({}, item, location)) {
+          location = isArray(results)? results.length : location;
+          results[location] = item;
+        }
+      }).apply({}, arguments);
+      return Object.freeze(results);
+    };
+  }
+
+  function reduce(memo){
+    return function(operation){
+      return function(){
+        each(function(item, index){
+          memo = operation(memo)(item);
+        }).apply({}, arguments);
+        return memo;
+      };
+    };
+  }
+
+// Object 
+
+  function extend(extention){
+    return function(obj){
+      var results = Object.create({});
+      eachArr(function(key){
+        results[key] = obj[key];
+      })(Object.keys(obj));
+      eachArr(function(key){
+        results[key] = extention[key];
+      })(Object.keys(extention));
+      return results;
+    };
+  }
+
+// Function operations
+
+  function compose(){
+    var funcs = arguments;
+    return function(){
+      var args = arguments;
+      eachArrRight(function(func){
+        args = [func.apply(this, args)];
+      })(funcs);
+      return args[0];
+    };
+  }
+
+  function not(func){
+    return function(){
+      var args = argsToList(arguments);
+      return !func.apply({}, args);
+    };
+  }
+
+  function dot(key){
+    return function(obj){
+      return obj[key];
+    };
+  }
+
+// Utilities
+
+  function expose(nameList){
+    var fNames = nameList.split(' ');
+    eachArr(function(fName){
+      window[fName] = _[fName];
+    })(fNames);
+  }
+
+  var _ =  {
+    map: map,
+    compose: compose,
+    not: not,
+    dot: dot,
+    expose: expose,
+    each: each,
+    eachObj: eachObj,
+    eachArr: eachArr,
+    // limit: limit,
+    extend: extend,
+    filter: filter,
+    reduce: reduce,
+    eachArrRight: eachArrRight
+  };
+  return _;
+}());
+(function(namespace){
+  function hasK(permitted){
+    permitted = argsToList(arguments);
+    return function(value, key){
+      return permitted.indexOf(key) !== -1;
+    };
+  }
+  function hasNotK(forbidden){
+    forbidden = argsToList(arguments);
+    return function(value, key){
+      return forbidden.indexOf(key) === -1;
+    };
+  }
+  function and(a){
+    return function(b){
+      return a && b;
+    };
+  }
+  
+  var limit = _.compose(_.filter, hasK);
+  var weed = _.compose(_.filter, hasNotK);
+  var pluck = _.compose(_.map, _.dot);
+
+  var every = _.reduce(true)(and);
+
+
+  namespace.limit = limit;
+  namespace.weed = weed;
+  namespace.every = every;
+  namespace.pluck = pluck;
+  namespace.and = and;
+  // weed = _.compose(_.filter, not(hasK));
+}(_));

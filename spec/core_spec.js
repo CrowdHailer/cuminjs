76f1d4911abd6d15397c619d4072ee464729e170
Peter <peterhsaxton@gmail.com>
2014-07-02 14:03:42 +0100

    reduced each tests

--- spec/core_spec.js
+++ spec/core_spec.js
@@ -9,26 +9,23 @@ describe('Cumin core functions', function () {
   _.expose('each ');
 
   ddescribe('each', function () {
-    it('should call every element over an array', function () {
-      each(dummy)([1, 2]);
-      expect(dummy).toHaveBeenCalledWith(1, 0);
-      expect(dummy).toHaveBeenCalledWith(2, 1);
+    it('should call the operation with every element and index of an array', function () {
+      each(dummy)([4, 2]);
+      expect(dummy.calls.allArgs()).toEqual([[4, 0], [2, 1]]);
     });
-    it('should call every value in an object', function () {
-      each(dummy)({x: 1, y: 2});
-      expect(dummy).toHaveBeenCalledWith(1, 'x');
-      expect(dummy).toHaveBeenCalledWith(2, 'y');
+    it('should call the operation with every value and key in an object', function () {
+      each(dummy)({x: 4, y: 2});
+      expect(dummy.calls.allArgs()).toEqual([[4, 'x'], [2, 'y']]);
     });
-    it('should call operation for a list of arguments', function () {
-      each(dummy)(1, 2);
-      expect(dummy).toHaveBeenCalledWith(1, 0);
-      expect(dummy).toHaveBeenCalledWith(2, 1);
+    it('should call the operation with every element and index in a list of arguments', function () {
+      each(dummy)(4, 2);
+      expect(dummy.calls.allArgs()).toEqual([[4, 0], [2, 1]]);
     });
-    it('should not call operation for an empty array', function () {
+    it('should not call the operation for an empty array', function () {
       each(dummy)([]);
       expect(dummy).not.toHaveBeenCalled();
     });
-    it('should not call operation for an empty object', function () {
+    it('should not call the operation for an empty object', function () {
       each(dummy)({});
       expect(dummy).not.toHaveBeenCalled();
     });

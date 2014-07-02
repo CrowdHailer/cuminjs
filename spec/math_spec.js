47105321f3996321f1fb1f9425ea3aa465059485
Peter <peterhsaxton@gmail.com>
2014-07-02 16:43:52 +0100

    wrap maths tests

--- spec/math_spec.js
+++ spec/math_spec.js
@@ -1,14 +1,18 @@
-describe('round', function(){
-  it('should round to integer', function(){
-    expect(_.round()(3.243)).toEqual(3);
-    expect(_.round()(3.743)).toEqual(4);
-  });
-  it('should round to integer number of decimals', function(){
-    expect(_.round(2)(3.243)).toEqual(3.24);
-    expect(_.round(2)(3.747)).toEqual(3.75);
-  });
-  it('should deal with edgecases well', function(){
-    expect(_.round(2)(1.005)).toEqual(1.01);
-    expect(_.round(2)(1.994)).toEqual(1.99);
+describe('Cumin maths library', function(){
+  'use strict';
+
+  describe('round', function(){
+    it('should round to integer', function(){
+      expect(_.round()(3.243)).toEqual(3);
+      expect(_.round()(3.743)).toEqual(4);
+    });
+    it('should round to integer number of decimals', function(){
+      expect(_.round(2)(3.243)).toEqual(3.24);
+      expect(_.round(2)(3.747)).toEqual(3.75);
+    });
+    it('should deal with edgecases well', function(){
+      expect(_.round(2)(1.005)).toEqual(1.01);
+      expect(_.round(2)(1.994)).toEqual(1.99);
+    });
   });
 });

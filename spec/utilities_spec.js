60d8d292effe1cd92de8ed52e4db2e087b17f840
Peter <peterhsaxton@gmail.com>
2014-07-04 13:20:41 +0100

    linted utilities spec

--- spec/utilities_spec.js
+++ spec/utilities_spec.js
@@ -1,28 +1,28 @@
-describe('Cumin utility operations', function(){
+describe('Cumin utility operations', function () {
   'use strict';
 
-  describe('expose', function(){
+  describe('expose', function () {
     // forceful, as, expose defaults such as filling missing keys with 0
-    it('should leave main namespace clear', function(){
-      expect(function(){
+    it('should leave main namespace clear', function () {
+      expect(function () {
         log();
       }).toThrowError("Can't find variable: log");
     });
-    it('should make functions available on top namespace', function(){
+    it('should make functions available on top namespace', function () {
       _.expose('equals dot method size now log defreeze refreeze');
-      expect(function(){
+      expect(function () {
         log();
       }).not.toThrow();
     });
   });
 
-  describe('equals', function(){
-    it('should test equality for values', function(){
+  describe('equals', function () {
+    it('should test equality for values', function () {
       var isThree = equals(3);
       expect(isThree(3)).toBe(true);
       expect(isThree(2)).toBe(false);
     });
-    it('can be used as a defined test', function(){
+    it('can be used as a defined test', function () {
       var a, b = 3;
       var defined = not(equals(undefined));
       expect(defined(a)).toBe(false);
@@ -30,97 +30,97 @@ describe('Cumin utility operations', function(){
     });
   });
 
-  describe('dot', function(){
-    it('should pull an objects value', function(){
+  describe('dot', function () {
+    it('should pull an objects value', function () {
       var person = {name: 'Mike'};
       var name = dot('name');
       expect(name(person)).toEqual('Mike');
     });
-    it('should return an array given an array', function(){
+    it('should return an array given an array', function () {
       var person = {name: 'Mike', age: 25, city: 'boston'};
       var publicDetails = dot(['name', 'city']);
       expect(publicDetails(person)).toEqual(Object.freeze(['Mike', 'boston']));
     });
-    it('should return an object given an object', function(){
+    it('should return an object given an object', function () {
       var person = {name: 'Mike', age: 25, city: 'boston'};
       var greeting = dot({hello: 'name', age: 'age'});
       expect(greeting(person)).toEqual(Object.freeze({hello: 'Mike', age: 25}));
     });
   });
 
-  describe('method', function(){
+  describe('method', function () {
     var name;
-    beforeEach(function(){
+    beforeEach(function () {
       name = method('getName');
     });
-    it('should get result of named method from and object', function(){
+    it('should get result of named method from and object', function () {
       var person = {getName: jasmine.createSpy().and.returnValue('Neil')};
       expect(name(person)).toEqual('Neil');
     });
-    it('should return undefined if there is no method', function(){
+    it('should return undefined if there is no method', function () {
       var person = {};
       expect(name(person)).toEqual();
     });
-    it('should return undefined when looking on undefined', function(){
+    it('should return undefined when looking on undefined', function () {
       expect(name()).toEqual();
     });
   });
 
-  describe('size', function(){
-    it('should return number of elements in an array', function(){
+  describe('size', function () {
+    it('should return number of elements in an array', function () {
       expect(size([1, 2])).toEqual(2);
     });
-    it('should return number of values on an object', function(){
+    it('should return number of values on an object', function () {
       expect(size({x: 1})).toEqual(1);
     });
   });
 
-  describe('now', function(){
-    it('should call date object', function(){
+  describe('now', function () {
+    it('should call date object', function () {
       _.expose('now');
       expect(now()).toEqual(Date.now());
     });
   });
 
-  describe('log', function(){
-    it('should log to console', function(){
+  describe('log', function () {
+    it('should log to console', function () {
       spyOn(console, 'log');
       log(3);
       expect(console.log).toHaveBeenCalledWith(3);
     });
   });
 
-  describe('defreeze and refreeze', function(){
-    beforeEach(function(){
+  describe('defreeze and refreeze', function () {
+    beforeEach(function () {
       defreeze();
     });
-    afterEach(function(){
+    afterEach(function () {
       refreeze();
     });
 
-    it('should work for mapArray', function(){
-      expect(mapArray(add3)([1, 2])).
-        toEqual([4, 5]);
+    it('should work for mapArray', function () {
+      expect(mapArray(add3)([1, 2]))
+        .toEqual([4, 5]);
     });
-    it('should work for mapObject', function(){
-      expect(mapObject(add3)({x: 1, y: 2})).
-        toEqual({x: 4, y: 5});
+    it('should work for mapObject', function () {
+      expect(mapObject(add3)({x: 1, y: 2}))
+        .toEqual({x: 4, y: 5});
     });
-    it('should work for filterArray', function(){
-      expect(filterArray(greaterThan2)([1, 3])).
-        toEqual([3]);
+    it('should work for filterArray', function () {
+      expect(filterArray(greaterThan2)([1, 3]))
+        .toEqual([3]);
     });
-    it('should work for filterObject', function(){
-      expect(filterObject(greaterThan2)({x: 1, y: 3})).
-        toEqual({y: 3});
+    it('should work for filterObject', function () {
+      expect(filterObject(greaterThan2)({x: 1, y: 3}))
+        .toEqual({y: 3});
     });
-    it('should work for foundation', function(){
-      expect(foundation({x: 1})({y: 2})).
-        toEqual({x: 1, y: 2});
+    it('should work for foundation', function () {
+      expect(foundation({x: 1})({y: 2}))
+        .toEqual({x: 1, y: 2});
     });
-    it('should work for overlay', function(){
-      expect(overlay({x: 1})({y: 2})).
-        toEqual({x: 1, y: 2});
+    it('should work for overlay', function () {
+      expect(overlay({x: 1})({y: 2}))
+        .toEqual({x: 1, y: 2});
     });
   });
 });
